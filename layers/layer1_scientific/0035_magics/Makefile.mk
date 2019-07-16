360d63bde05d387492b927ecce36f1a7883ba9f9
thebaptiste <jean-baptiste.veslin@meteo.fr>
2019-07-16 13:54:23 +0200

    feat: upgrade Magics to 4.0 and build python2/3 api with pypi

---
+++ layers/layer1_scientific/0035_magics/Makefile.mk
@@ -0,0 +1,35 @@
+include ../../../adm/root.mk
+include $(MFEXT_HOME)/share/package.mk
+
+export NAME=Magics
+export VERSION=4.1.0
+export EXTENSION=tar.gz
+export CHECKTYPE=MD5
+export CHECKSUM=eb0f5bbfd13c4a7d44fc0089b6b71999
+export EXPLICIT_NAME=$(NAME)-$(VERSION)-Source
+export SCIENTIFIC_ROOT=$(PREFIX)/../scientific_core
+#Python (Jinja2) is required for build, even if we don t build python api
+export PYTHONPATH=$(PREFIX)/../python3/lib/python3.7/site-packages:$(PREFIX)/../python3_core/lib/python3.7/site-packages
+export PYTHON=$(PREFIX)/../python3_core/bin/python
+DESCRIPTION=\
+Magics is the latest generation of the ECMWF's meteorological plotting software.
+WEBSITE=https://www.ecmwf.int/en/computing/software
+LICENSE=Apache 2.0 : http://www.apache.org/licenses/LICENSE-2.0
+
+#We need C++14 features to build
+GCC_VERSION = `gcc --version | head -1 | cut -d" " -f3 | cut -d"." -f1-2`
+DEVTOOLSET = 7
+
+ifeq ($(shell expr $(GCC_VERSION) \< "4.9" ), 1)
+
+all:: $(PREFIX)/bin/magmlx
+$(PREFIX)/bin/magmlx:
+	scl enable devtoolset-$(DEVTOOLSET) '$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) EXPLICIT_NAME="$(EXPLICIT_NAME)" OPTIONS="-DENABLE_PYTHON=OFF -DENABLE_FORTRAN=OFF -DNETCDF_PATH=$(SCIENTIFIC_ROOT) -DPROJ4_PATH=$(SCIENTIFIC_ROOT)" download uncompress configure_cmake3 build_cmake install_cmake'
+
+else
+
+all:: $(PREFIX)/bin/magmlx
+$(PREFIX)/bin/magmlx:
+	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) EXPLICIT_NAME="$(EXPLICIT_NAME)" OPTIONS="-DENABLE_PYTHON=OFF -DENABLE_FORTRAN=OFF -DNETCDF_PATH=$(SCIENTIFIC_ROOT) -DPROJ4_PATH=$(SCIENTIFIC_ROOT)" download uncompress configure_cmake3 build_cmake install_cmake
+
+endif
