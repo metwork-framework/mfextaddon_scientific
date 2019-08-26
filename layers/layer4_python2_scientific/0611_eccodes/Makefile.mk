c82317b6da68d8953040b0733641e342a34cfc6d
thebaptiste <jean-baptiste.veslin@meteo.fr>
2019-08-26 15:33:12 +0200

    build: fix build for python2 eccodes

--- layers/layer4_python2_scientific/0611_eccodes/Makefile.mk
+++ layers/layer4_python2_scientific/0611_eccodes/Makefile.mk
@@ -18,8 +18,8 @@ LICENSE=Apache 2.0 : http://www.apache.org/licenses/LICENSE-2.0
 
 all:: $(PREFIX)/lib/python$(PYTHON2_SHORT_VERSION)/site-packages/eccodes-$(VERSION)-py$(PYTHON2_SHORT_VERSION).egg-info
 $(PREFIX)/lib/python$(PYTHON2_SHORT_VERSION)/site-packages/eccodes-$(VERSION)-py$(PYTHON2_SHORT_VERSION).egg-info:
+	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) EXPLICIT_NAME="$(EXPLICIT_NAME)" OPTIONS="-DBUILD_SHARED_LIBS=ON -DCMAKE_PREFIX_PATH='$(PREFIX);$(PREFIX)/../scientific_core' -DENABLE_FORTRAN=OFF -DENABLE_NETCDF=OFF -DENABLE_PYTHON=ON -DENABLE_JPG=ON -DOPENJPEG_PATH=$(PREFIX)/../scientific_core" download uncompress configure_cmake3
 	mkdir -p build/$(NAME)-$(VERSION)-Source/build/lib
-	rm -f build/$(NAME)-$(VERSION)-Source/build/lib/libeccodes.so
 	ln -s $(PREFIX)/../scientific/lib/libeccodes.so build/$(NAME)-$(VERSION)-Source/build/lib/libeccodes.so
-	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) EXPLICIT_NAME="$(EXPLICIT_NAME)" OPTIONS="-DBUILD_SHARED_LIBS=ON -DCMAKE_PREFIX_PATH='$(PREFIX);$(PREFIX)/../scientific_core' -DENABLE_FORTRAN=OFF -DENABLE_NETCDF=OFF -DENABLE_PYTHON=ON -DENABLE_JPG=ON -DOPENJPEG_PATH=$(PREFIX)/../scientific_core" download uncompress configure_cmake3 python2pyinstall_cmake
+	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) EXPLICIT_NAME="$(EXPLICIT_NAME)" OPTIONS="-DBUILD_SHARED_LIBS=ON -DCMAKE_PREFIX_PATH='$(PREFIX);$(PREFIX)/../scientific_core' -DENABLE_FORTRAN=OFF -DENABLE_NETCDF=OFF -DENABLE_PYTHON=ON -DENABLE_JPG=ON -DOPENJPEG_PATH=$(PREFIX)/../scientific_core" python2pyinstall_cmake
 	rm -f build/$(NAME)-$(VERSION)-Source/build/lib/libeccodes.so
