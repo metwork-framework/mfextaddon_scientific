576be68a929249bebf452b17e9d6a8caf00d30b5
thebaptiste <jean-baptiste.veslin@meteo.fr>
2019-09-05 14:40:17 +0200

    feat: add layer scientific_system_libraries (for build issues)
    Close: #43

---
+++ layers/layer0_scientific_system_libraries/0001_libraries/Makefile.mk
@@ -0,0 +1,12 @@
+include ../../../adm/root.mk
+include $(MFEXT_HOME)/share/simple_layer.mk
+
+ifeq ($(shell cat /etc/redhat-release |grep -c "CentOS release 6"), 1)
+
+LIB_LIST = $(shell cat libraries.txt)
+$(shell mkdir $(PREFIX)/lib)
+
+all:: $(PREFIX)/lib/libgfortran.so.3
+$(PREFIX)/lib/libgfortran.so.3:
+	for lib in $(LIB_LIST); do cp -r $$lib $(PREFIX)/lib; done
+endif
