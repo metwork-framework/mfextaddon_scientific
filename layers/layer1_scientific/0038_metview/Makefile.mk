c784d87c64bee38835c9c89f85da1f1ae9e9d689
thebaptiste <jean-baptiste.veslin@meteo.fr>
2020-12-10 16:08:04 +0100

    feat: upgrade eccodes to 2.19.1, Magics to 4.5.1 and Metview to 5.10.1 (for compatibily with the python bindings)

--- layers/layer1_scientific/0038_metview/Makefile.mk
+++ layers/layer1_scientific/0038_metview/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include $(MFEXT_HOME)/share/package.mk
 
 export NAME=Metview
-export VERSION=5.7.2.1
+export VERSION=5.10.1
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=bc6db8de64e80b49bd089ab0b550ec5e
+export CHECKSUM=a4feb64435d21463442202d1e9b1f247
 export EXPLICIT_NAME=$(NAME)-$(VERSION)-Source
 DESCRIPTION=\
 Metview is a meteorological workstation application designed to be a complete \
@@ -16,4 +16,4 @@ LICENSE=Apache 2.0 : http://www.apache.org/licenses/LICENSE-2.0
 
 all:: $(PREFIX)/bin/metview
 $(PREFIX)/bin/metview:
-	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) EXPLICIT_NAME="$(EXPLICIT_NAME)" OPTIONS="-DCMAKE_INSTALL_PREFIX=$(PREFIX) -DCMAKE_PREFIX_PATH=\"$(PREFIX);$(PREFIX)/../scientific_core;$(PREFIX)/../core\" -DENABLE_UI=OFF -DENABLE_METVIEW_FORTRAN=OFF -DNETCDF_PATH=$(PREFIX)/../scientific_core" download uncompress configure_cmake3 build_cmake install_cmake
+	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) EXPLICIT_NAME="$(EXPLICIT_NAME)" OPTIONS="-DCMAKE_INSTALL_PREFIX=$(PREFIX) -DCMAKE_PREFIX_PATH='$(PREFIX);$(PREFIX)/../scientific_core;$(PREFIX)/../core' -DENABLE_UI=OFF -DENABLE_METVIEW_FORTRAN=OFF -DNETCDF_PATH=$(PREFIX)/../scientific_core" download uncompress configure_cmake3 build_cmake install_cmake
