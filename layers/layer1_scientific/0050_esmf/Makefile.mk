0a3823adf518d73b6db6618a6b7fde69fdeebea6
Jean-Baptiste VESLIN <jbaptiste31@free.fr>
2021-03-08 15:02:27 +0100

    build: fix access url, tar name and checksum for esmf 7_1_0r (#194)

--- layers/layer1_scientific/0050_esmf/Makefile.mk
+++ layers/layer1_scientific/0050_esmf/Makefile.mk
@@ -5,12 +5,12 @@ export NAME=esmf
 export VERSION=7_1_0r
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=9e455bc36a0aaa9b87e0bdedc78a47f5
+export CHECKSUM=980da40d07ae6393ed8551790d7c282e
 DESCRIPTION=\
 The Earth System Modeling Framework (ESMF) is a suite of software tools for developing high-performance, multi-component Earth science modeling applications. Such applications may include a few or dozens of components representing atmospheric, oceanic, terrestrial, or other physical domains, and their constituent processes (dynamical, chemical, biological, etc.).
 WEBSITE=http://www.earthsystemmodeling.org
 LICENSE=NCSA Open Source License (https://opensource.org/licenses/NCSA)
-export EXPLICIT_NAME=$(NAME)
+export EXPLICIT_NAME=esmf-ESMF_$(VERSION)
 
 export ESMF_DIR=$(CURDIR)/build/$(EXPLICIT_NAME)
 export ESMF_INSTALL_PREFIX=$(PREFIX)/$(NAME)
