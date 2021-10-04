7503bd05f60af0cc83168da137810ae380394d6a
thebaptiste <jean-baptiste.veslin@meteo.fr>
2021-10-04 17:34:57 +0200

    feat: remove graphviz (now in layer core@mfext) and upgrade pygraphviz from 1.5 to 1.6 (python 3.9)

--- layers/layer4_python3_scientific/0505_pygraphviz/Makefile.mk
+++ layers/layer4_python3_scientific/0505_pygraphviz/Makefile.mk
@@ -4,10 +4,10 @@ include $(MFEXT_HOME)/share/package.mk
 #pygraphviz doesn't build correctly with pypi when library graphviz is built
 #within mfext. So we build it separately (not in 0500_extra_packages)
 export NAME=pygraphviz
-export VERSION=1.5
+export VERSION=1.6
 export EXTENSION=zip
 export CHECKTYPE=MD5
-export CHECKSUM=c186f5f6567e523a862063fc62ddcd2f
+export CHECKSUM=4fd63c6feccb3c80be3cf640fe8285f0
 DESCRIPTION=\
 PYGRAPHVIZ is a Python interface to Graphviz
 WEBSITE=http://pygraphviz.github.io/
