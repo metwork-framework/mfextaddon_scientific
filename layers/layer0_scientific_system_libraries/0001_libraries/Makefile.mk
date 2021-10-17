44cf8cbd7ef2912650654f9711864349e7e0bb76
Jean-Baptiste VESLIN <jbaptiste31@free.fr>
2021-10-17 09:05:30 +0200

    build: fix for CentOS 8 build

--- layers/layer0_scientific_system_libraries/0001_libraries/Makefile.mk
+++ layers/layer0_scientific_system_libraries/0001_libraries/Makefile.mk
@@ -6,7 +6,7 @@ ifeq ($(shell cat /etc/redhat-release | grep -c "CentOS" | grep -c "release 8"),
 LIB_LIST = $(shell cat libraries.txt)
 $(shell mkdir $(PREFIX)/lib)
 
-all:: $(PREFIX)/lib/libgfortran.so.3
-$(PREFIX)/lib/libgfortran.so.3:
+all:: $(PREFIX)/lib/libopenblas.so.0
+$(PREFIX)/lib/libopenblas.so.0:
 	for lib in $(LIB_LIST); do cp -r $$lib $(PREFIX)/lib; done
 endif
