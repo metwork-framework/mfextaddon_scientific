6659724da13c38eaba092bbce7fad280cedf622a
Jean-Baptiste VESLIN <jbaptiste31@free.fr>
2022-01-07 15:29:18 +0100

    feat: build on centos8 (#280)
    * build: test on centos8
    
    * build: build on centos8
    
    * build: atlas is required in centos8
    
    * build: add dependency libtatlas.so on centos8
    
    * build: fix cartopy url
    
    * build: atlas is not required anymore
    
    * build: fix for CentOS8 build
    
    * build: fortran libs are provided by distribution on CentOS 8 build
    
    * build: fix for CentOS 8 build
    
    * build: fix
    
    * build: fix librarie name on CentOS8

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -15,6 +15,7 @@ export DRONE=true
 rm -rf html_doc rpms .build_hash
 
 
+
     if test "${OS_VERSION}" = "centos8"; then export METWORK_BUILD_OS=generic; else export METWORK_BUILD_OS=${OS_VERSION}; fi
 
 
--- layers/layer0_scientific_system_libraries/0001_libraries/Makefile.mk
+++ layers/layer0_scientific_system_libraries/0001_libraries/Makefile.mk
@@ -1,12 +1,12 @@
 include ../../../adm/root.mk
 include $(MFEXT_HOME)/share/simple_layer.mk
 
-ifeq ($(shell cat /etc/redhat-release |grep -c "CentOS release 6"), 1)
+ifeq ($(shell cat /etc/redhat-release | grep -c "release 8"), 1)
 
 LIB_LIST = $(shell cat libraries.txt)
 $(shell mkdir $(PREFIX)/lib)
 
-all:: $(PREFIX)/lib/libgfortran.so.3
-$(PREFIX)/lib/libgfortran.so.3:
+all:: $(PREFIX)/lib/libopenblas.so.0
+$(PREFIX)/lib/libopenblas.so.0:
 	for lib in $(LIB_LIST); do cp -r $$lib $(PREFIX)/lib; done
 endif
