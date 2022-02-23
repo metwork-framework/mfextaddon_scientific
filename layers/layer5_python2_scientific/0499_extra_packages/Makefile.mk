a6206a0c046e4853efea5d206242f259ff7bc41d
thebaptiste <jbaptiste31@free.fr>
2022-02-23 20:50:21 +0000

    feat: upgrade pycurl to 7.44.1 for compatibility with system libcurl

--- layers/layer5_python2_scientific/0499_extra_packages/Makefile.mk
+++ layers/layer5_python2_scientific/0499_extra_packages/Makefile.mk
@@ -2,5 +2,6 @@ include ../../../adm/root.mk
 
 export HDF5_DIR=$(PREFIX)/../scientific_core
 export GEOS_DIR=$(PREFIX)/../scientific_core
+export PYCURL_SSL_LIBRARY=openssl
 
 include $(MFEXT_HOME)/share/python2_layer.mk
