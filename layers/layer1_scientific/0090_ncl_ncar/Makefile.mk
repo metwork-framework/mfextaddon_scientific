fdec6462850c6c5f04d8c81dcf9920696b0f8de5
Jean-Baptiste VESLIN <jbaptiste31@free.fr>
2022-02-24 19:20:08 +0100

    build: improve ncl/ncarg build (#310)

--- layers/layer1_scientific/0090_ncl_ncar/Makefile.mk
+++ layers/layer1_scientific/0090_ncl_ncar/Makefile.mk
@@ -12,9 +12,15 @@ NCL (NCAR Command Language) is a scripting language for the analysis and visuali
 WEBSITE=https://www.ncl.ucar.edu/
 LICENSE=Apache 2.0
 
+RPM_OPT_FLAGS=$(shell outside rpm -E %optflags)
+FCOPTIONS=$(RPM_OPT_FLAGS) -fPIC -fno-second-underscore -fno-range-check -fopenmp
+CCOPTIONS=$(RPM_OPT_FLAGS) -std=c99 -fPIC -fno-strict-aliasing -fopenmp -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H -fcommon
+
 all::$(PREFIX)/bin/ncl
 $(PREFIX)/bin/ncl:
 	#inspired by ncl.spec in https://download-ib01.fedoraproject.org/pub/epel/8/Everything/SRPMS/Packages/n/ncl-6.6.2-12.el8.src.rpm
+	mkdir -p $(PREFIX)/share/ncarg
 	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) EXPLICIT_NAME="$(EXPLICIT_NAME)" download uncompress
 	cp Site.local build/$(EXPLICIT_NAME)/config/Site.local
-	cd build/$(EXPLICIT_NAME) && ./config/ymkmf && make Build && make install
+	cd build/$(EXPLICIT_NAME) && ./config/ymkmf && make Build CCOPTIONS="$(CCOPTIONS)" F77=gfortran F77_LD=gfortran CTOFLIBS="-lgfortran" FCOPTIONS="$(FCOPTIONS)" COPT= FOPT= && make install
+	cd $(PREFIX)/lib/ncarg && rm -rf udunits && ln -s $(PREFIX)/share/udunits udunits
