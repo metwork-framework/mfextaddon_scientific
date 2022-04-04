fa4f4d5b37a6b689125a04187578d492446f622e
thebaptiste <jean-baptiste.veslin@meteo.fr>
2022-04-04 15:50:04 +0000

    build: fix g2clib build

--- layers/layer1_scientific/0089_g2clib/Makefile.mk
+++ layers/layer1_scientific/0089_g2clib/Makefile.mk
@@ -3,16 +3,15 @@ include $(MFEXT_HOME)/share/package.mk
 
 export NAME=g2clib
 export VERSION=1.6.0
-export EXTENSION=tar.gz
+export EXTENSION=tar.bz2
 export CHECKTYPE=MD5
-export CHECKSUM=11c1244e03f6278180b399e27f86468e
+export CHECKSUM=89a6df22cf1362d284d04873c5d0b8ce
 DESCRIPTION=\
 G2CLIB contains "C" decoder/encoder routines for GRIB edition 2
 WEBSITE=https://www.ncl.ucar.edu/
 LICENSE=Apache 2.0
-EXPLICIT_NAME=$(NAME)-$(VERSION)-patch
 
 all::$(PREFIX)/lib/libgrib2c.a
 $(PREFIX)/lib/libgrib2c.a:
-	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) EXPLICIT_NAME="$(EXPLICIT_NAME)" download uncompress build
-	cd build/$(EXPLICIT_NAME) && cp libgrib2c.a $(PREFIX)/lib && cp *.h $(PREFIX)/include
+	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) download uncompress build
+	cd build/$(NAME)-$(VERSION) && cp libg2c_v1.6.0.a $(PREFIX)/lib/libgrib2c.a && cp *.h $(PREFIX)/include
