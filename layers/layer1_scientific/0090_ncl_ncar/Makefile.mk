d7f8bc7a8a3aa2bb046e89dd7b1c49026e5b78ec
thebaptiste <jean-baptiste.veslin@meteo.fr>
2022-04-07 14:13:41 +0000

    build: fix ncl build

--- layers/layer1_scientific/0090_ncl_ncar/Makefile.mk
+++ layers/layer1_scientific/0090_ncl_ncar/Makefile.mk
@@ -1,12 +1,11 @@
 include ../../../adm/root.mk
 include $(MFEXT_HOME)/share/package.mk
 
-export NAME=ncl_ncar
+export NAME=ncl
 export VERSION=6.6.2
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=dd36ef49b0be92b0142795059009d766
-EXPLICIT_NAME=$(NAME)g-$(VERSION)
+export CHECKSUM=38e3d2b55490f6eac93035d31579a45e
 DESCRIPTION=\
 NCL (NCAR Command Language) is a scripting language for the analysis and visualization of climate and weather data
 WEBSITE=https://www.ncl.ucar.edu/
@@ -20,7 +19,7 @@ all::$(PREFIX)/bin/ncl
 $(PREFIX)/bin/ncl:
 	#inspired by ncl.spec in https://download-ib01.fedoraproject.org/pub/epel/8/Everything/SRPMS/Packages/n/ncl-6.6.2-12.el8.src.rpm
 	mkdir -p $(PREFIX)/share/ncarg
-	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) EXPLICIT_NAME="$(EXPLICIT_NAME)" download uncompress
-	cp Site.local build/$(EXPLICIT_NAME)/config/Site.local
-	cd build/$(EXPLICIT_NAME) && ./config/ymkmf && make Build CCOPTIONS="$(CCOPTIONS)" F77=gfortran F77_LD=gfortran CTOFLIBS="-lgfortran" FCOPTIONS="$(FCOPTIONS)" COPT= FOPT= && make install
+	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) download uncompress
+	cp Site.local build/$(NAME)-$(VERSION)/config/Site.local
+	cd build/$(NAME)-$(VERSION) && ./config/ymkmf && make Build CCOPTIONS="$(CCOPTIONS)" F77=gfortran F77_LD=gfortran CTOFLIBS="-lgfortran" FCOPTIONS="$(FCOPTIONS)" COPT= FOPT= && make install
 	cd $(PREFIX)/lib/ncarg && rm -rf udunits && ln -s $(PREFIX)/share/udunits udunits
