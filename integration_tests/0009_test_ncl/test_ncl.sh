7d16cb05ecbd1c0b077d11855af09f2559ed975c
Jean-Baptiste VESLIN <jbaptiste31@free.fr>
2022-04-11 10:18:56 +0200

    build: fix ncl test (#337)

--- integration_tests/0009_test_ncl/test_ncl.sh
+++ integration_tests/0009_test_ncl/test_ncl.sh
@@ -11,6 +11,6 @@ ncargpath root
 
 echo ""
 echo "test grib edition 2"
-ncl_filedump  ../0005_cdo_read_grib2/fic_grib2_test.grib
+ncl_filedump ../0005_cdo_read_grib2/fic_grib2_test.grib
 
-ncl_filedump  ./GRIBASPOCEURW1S100Top20210506120000_75.grib
+ncl_filedump ./GRIBASPOCEURW1S100Top20210506120000_75.grib
