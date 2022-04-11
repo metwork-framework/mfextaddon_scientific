19b065ce5b02b5f5aabaf33a12392d0668a2d141
thebaptiste <jean-baptiste.veslin@meteo.fr>
2022-04-11 08:16:39 +0000

    build: fix ncl tests

--- integration_tests/0009_test_ncl/test_ncl.sh
+++ integration_tests/0009_test_ncl/test_ncl.sh
@@ -8,3 +8,9 @@ ncl -V
 echo ""
 echo "NCARG_ROOT : "
 ncargpath root
+
+echo ""
+echo "test grib edition 2"
+ncl_filedump ../0005_cdo_read_grib2/fic_grib2_test.grib
+
+ncl_filedump ./GRIBASPOCEURW1S100Top20210506120000_75.grib
