64a737cf862d967d180165d37b8dfc5a12a6f846
metworkbot <metworkbot@metwork-framework.org>
2022-05-31 14:31:40 +0000

    build: sync common files from github_organization_management repository

--- bootstrap.sh
+++ bootstrap.sh
@@ -83,7 +83,6 @@ fi
 
 # FIXME: do not hardcode this
 # FIXME: move to layer root extra_env ?
-echo "export PYTHON2_SHORT_VERSION := 2.7" >>adm/root.mk
 echo "export PYTHON3_SHORT_VERSION := 3.9" >>adm/root.mk
 
 echo "BOOTSTRAP DONE !"
