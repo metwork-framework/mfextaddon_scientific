c5ac7b093db1d00bf7fa86c3f2a9f496d89f512f
Jean-Baptiste VESLIN <jbaptiste31@free.fr>
2022-06-16 16:06:08 +0200

    build: remove useless patch since cartopy upgrade to 0.20.2 (#370)

--- layers/layer4_python3_scientific/0700_fix_packages/Makefile.mk
+++
@@ -1,7 +0,0 @@
-include ../../../adm/root.mk
-include $(MFEXT_HOME)/share//simple_layer.mk
-
-all:: $(PREFIX)/lib/python$(PYTHON3_SHORT_VERSION)/site-packages/cartopy/io/.url_naturalearth_fixed
-
-$(PREFIX)/lib/python$(PYTHON3_SHORT_VERSION)/site-packages/cartopy/io/.url_naturalearth_fixed:
-	cd $(PREFIX)/lib/python$(PYTHON3_SHORT_VERSION)/site-packages/cartopy/io/ && cat shapereader.py | sed -e 's/naciscdn.org\/naturalearth\/{resolution}/naturalearth.s3.amazonaws.com\//' | sed -e 's/\/{category}/{resolution}_{category}/' > shapereader.py.new && mv shapereader.py.new shapereader.py && touch .url_naturalearth_fixed
