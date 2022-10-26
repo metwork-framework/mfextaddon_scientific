d89ce6cdcf5977a0de0e7621df2b981452c84d1f
Jean-Baptiste VESLIN <jbaptiste31@free.fr>
2022-10-26 15:20:03 +0200

    feat: add python3 plugin for uwsgi (#412)

---
+++ layers/layer4_python3_scientific/0640_uwsgi_plugin/Makefile.mk
@@ -0,0 +1,19 @@
+include ../../../adm/root.mk
+include $(MFEXT_HOME)/share/package.mk
+
+export NAME=uwsgi-plugin-python3
+export VERSION=2.0.20
+DESCRIPTION=\
+Python 3.10 plugin for UWSGI
+WEBSITE=https://github.com/unbit/uwsgi
+LICENSE=GPL2+
+
+#We do not provide uwsgi but only python3_plugin.so based on Metwork python3
+#This plugin can be added to /usr/lib64/uwsgi to be used with Metwork's python 3.10
+#instead of the plugin for OS's python3 provided by the rpm uwsgi-plugin-python3
+all:: $(PREFIX)/lib/uwsgi/python3_plugin.so
+$(PREFIX)/lib/uwsgi/python3_plugin.so:
+	/usr/sbin/uwsgi --build-plugin "/usr/src/uwsgi/2.0.20/plugins/python/ python3"
+	mkdir -p $(PREFIX)/lib/uwsgi
+	mv python3_plugin.so $(PREFIX)/lib/uwsgi
+
