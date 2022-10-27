71cb10a814ae38083f15522e3d521e771e922cd5
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2022-10-27 19:08:57 +0200

    build: fix ci (#414)

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -50,8 +50,8 @@ if test -f /opt/metwork-mfext-${TARGET_DIR}/.dhash; then cat /opt/metwork-mfext-
 cat module_hash.debug |sort |uniq ; rm -f module_hash.debug
 echo "${MODULEHASH}${DRONE_TAG}${DRONE_BRANCH}" |md5sum |cut -d ' ' -f1 >.build_hash
 if test -f "${BUILDCACHE}/build_hash_mfextaddon_scientific_${BRANCH}_`cat .build_hash`"; then
-    echo "::set-output name=bypass::true"
-    echo "::set-output name=buildcache::null"
+    echo "bypass=true" >> github_output
+    echo "buildcache=null" >> github_output
     exit 0
 fi
  
@@ -73,7 +73,6 @@ touch ${hash_file}
 rm -f ./build_hash
 chown 1018:1018 ${hash_file}
 chmod 664 ${hash_file}
-echo "::set-output name=buildcache::${hash_file}"
+echo "buildcache=${hash_file}" >> github_output
  
-
-echo "::set-output name=bypass::false"
+echo "bypass=false" >> github_output
