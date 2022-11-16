831e1d1e80cd5185a0dad3c758607435b2f2aa36
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2022-11-16 10:50:12 +0100

    feat: upgrade pngquant, nco and netcdf_fortran (#424)

--- layers/layer1_scientific/0004_pngquant/Makefile.mk
+++ layers/layer1_scientific/0004_pngquant/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include $(MFEXT_HOME)/share/package.mk
 
 export NAME=pngquant
-export VERSION=2.12.5
+export VERSION=2.17.0
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=e2a02de064d6887d563fa12ceb1eb775
+export CHECKSUM=e18dd9cc2114c28f85b04b376512c267
 DESCRIPTION=\
 PNGQUANT is a command-line utility and a library for lossy compression of PNG images
 WEBSITE=http://www.pngquant.org/
--- layers/layer1_scientific/0015_nco/Makefile.mk
+++ layers/layer1_scientific/0015_nco/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include $(MFEXT_HOME)/share/package.mk
 
 export NAME=nco
-export VERSION=4.8.1
+export VERSION=5.1.1
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=547209b355cc20b87f6e093fbf810557
+export CHECKSUM=aeab69ac52334f68b668909851f25a25
 DESCRIPTION=\
 The netCDF Operators, NCO, are a suite of programs known as operators. \
 The operators facilitate manipulation and analysis of self-describing\
@@ -15,4 +15,4 @@ LICENSE=GNU General Public License
 
 all:: $(PREFIX)/lib/libnco.so
 $(PREFIX)/lib/libnco.so:
-	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) EXTRALDFLAGS="-L$(PREFIX)/lib -L$(PREFIX)/../scientific_core/lib" EXTRACFLAGS="-I$(PREFIX)/include -I$(PREFIX)/../scientific_core/include" OPTIONS="--enable-static=no" download uncompress configure build install
+	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) EXTRALDFLAGS="-L$(PREFIX)/lib -L$(PREFIX)/../scientific_core/lib" EXTRACFLAGS="-I$(PREFIX)/include -I$(PREFIX)/../scientific_core/include" OPTIONS="--enable-static=no --enable-nco_cplusplus=yes" download uncompress configure build install
