9f962b86f19daefc8b2a2ef116a45f1451da77ba
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2023-03-09 17:37:49 +0100

    feat: upgrade esmf to 8.4.1 and simplify our patch (#456)

--- layers/layer4_python3_scientific/0630_esmf/Makefile.mk
+++ layers/layer4_python3_scientific/0630_esmf/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include $(MFEXT_HOME)/share/package_python3.mk
 
 export NAME=esmf
-export VERSION=8.4.0
+export VERSION=8.4.1
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=b3b00ae4f7a7d37add2c481ecd564dff
+export CHECKSUM=5b9ea69c3be25ca14173ef9e16d06f2b
 DESCRIPTION=\
 Earth System Modeling Framework (ESMF) Python Interface (ESMPy)
 WEBSITE=http://www.earthsystemmodeling.org
@@ -26,4 +26,4 @@ export ESMFMKFILE=$(ESMF_INSTALL_LIBDIR)/esmf.mk
 all:: $(PYTHON3_SITE_PACKAGES)/esmpy-$(VERSION).dist-info
 $(PYTHON3_SITE_PACKAGES)/esmpy-$(VERSION).dist-info:
 	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) download uncompress
-	cd build/$(NAME)-$(VERSION)/src/addon/esmpy && unsafe_pip install --prefix=$(PREFIX) --src=$(PYTHON3_SITE_PACKAGES) . && rm -f $(PYTHON3_SITE_PACKAGES)/esmpy-$(VERSION).dist-info/direct_url.json
+	cd build/$(NAME)-$(VERSION)/src/addon/esmpy && unsafe_pip install --no-build-isolation --prefix=$(PREFIX) --src=$(PYTHON3_SITE_PACKAGES) . && rm -f $(PYTHON3_SITE_PACKAGES)/esmpy-$(VERSION).dist-info/direct_url.json
