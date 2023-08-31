5146b85e90ee2f8e574b35c0889e96ea4aaee567
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2023-08-31 17:27:54 +0200

    feat: upgrade ecbuild to 3.8.0 and eckit to 1.24.4 (#525)

--- layers/layer1_scientific/0027_ecbuild/Makefile.mk
+++ layers/layer1_scientific/0027_ecbuild/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include $(MFEXT_HOME)/share/package.mk
 
 export NAME=ecbuild
-export VERSION=3.7.0
+export VERSION=3.8.0
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=f1c45426c586cc6dbb7d3d5fe6d4761f
+export CHECKSUM=00bac5a0e01b01a52d1a8ffe040a3789
 DESCRIPTION= ecBuild is built on top of CMake and consists of a set of macros as well as a wrapper around CMake
 WEBSITE=https://github.com/ecmwf/ecbuild
 LICENSE=Apache 2.0 : http://www.apache.org/licenses/LICENSE-2.0
