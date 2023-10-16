2b7741e9127ea432569dbcc40e32969f2aac04c8
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2023-10-16 10:39:00 +0200

    feat: upgrade cdo to 2.2.0 and its python binding to 1.6.0 (#551)

--- layers/layer1_scientific/0060_cdo/Makefile.mk
+++ layers/layer1_scientific/0060_cdo/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include $(MFEXT_HOME)/share/package.mk
 
 export NAME=cdo
-export VERSION=2.1.0
+export VERSION=2.2.2
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=a926637d6d140d492c34ab24385d8c4d
+export CHECKSUM=e8e0989e2e1d3a04c59855121d7f8225
 DESCRIPTION=\
 CDO is a collection of command line Operators to manipulate and analyse Climate and NWP model Data.\
 Supported data formats are GRIB 1/2, netCDF 3/4, SERVICE, EXTRA and IEG. There are more than 600 \
