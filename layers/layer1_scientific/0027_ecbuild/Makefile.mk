e87488d58e01fb10fceb61f1dcf19fab41e51ff8
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2023-12-19 17:15:20 +0100

    feat: upgrade eccodes to 2.33.0, Magics to 4.15.0, Metview to 5.21.0 (#585)
    (cherry picked from commit 822b1bc17d5a92045c4b409fd550da45ab889715)

--- layers/layer1_scientific/0027_ecbuild/Makefile.mk
+++ layers/layer1_scientific/0027_ecbuild/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include $(MFEXT_HOME)/share/package.mk
 
 export NAME=ecbuild
-export VERSION=3.8.1
+export VERSION=3.8.2
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=e8f8ac3ecdf416b2824342cb787eede6
+export CHECKSUM=bd1199e72a47cc7375fe7822e3ed0387
 DESCRIPTION= ecBuild is built on top of CMake and consists of a set of macros as well as a wrapper around CMake
 WEBSITE=https://github.com/ecmwf/ecbuild
 LICENSE=Apache 2.0 : http://www.apache.org/licenses/LICENSE-2.0
