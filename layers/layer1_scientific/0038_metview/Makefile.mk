5bfa350c370f1a066bc8e3aa34639fbbef5d4c48
thebaptiste <jean-baptiste.veslin@meteo.fr>
2024-06-19 09:19:30 +0000

    build: build Metwiew with built-in libtirpc

--- layers/layer1_scientific/0038_metview/Makefile.mk
+++ layers/layer1_scientific/0038_metview/Makefile.mk
@@ -16,4 +16,4 @@ LICENSE=Apache 2.0 : http://www.apache.org/licenses/LICENSE-2.0
 
 all:: $(PREFIX)/bin/metview
 $(PREFIX)/bin/metview:
-	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) EXPLICIT_NAME="$(EXPLICIT_NAME)" OPTIONS="-DCMAKE_INSTALL_PREFIX=$(PREFIX) -DCMAKE_PREFIX_PATH='$(PREFIX);$(PREFIX)/../scientific_core;$(PREFIX)/../core' -DENABLE_UI=OFF -DENABLE_FORTRAN=ON -DENABLE_METVIEW_FORTRAN=ON -DNETCDF_PATH=$(PREFIX)/../scientific_core -DRPC_INCLUDE_DIR=$(PREFIX)/../core/include/tirpc -DRPC_LIBRARY=$(PREFIX)/../core/lib" download uncompress configure_cmake3 build_cmake install_cmake
+	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) EXPLICIT_NAME="$(EXPLICIT_NAME)" EXTRALDFLAGS="-L$(PREFIX)/../core/lib -ltirpc" OPTIONS="-DCMAKE_INSTALL_PREFIX=$(PREFIX) -DCMAKE_PREFIX_PATH='$(PREFIX);$(PREFIX)/../scientific_core;$(PREFIX)/../core' -DENABLE_UI=OFF -DENABLE_FORTRAN=ON -DENABLE_METVIEW_FORTRAN=ON -DNETCDF_PATH=$(PREFIX)/../scientific_core -DRPC_INCLUDE_DIR=$(PREFIX)/../core/include/tirpc -DRPC_LIBRARY=$(PREFIX)/../core/lib" download uncompress configure_cmake3 build_cmake install_cmake
