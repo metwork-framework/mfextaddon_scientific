e6643cfe8bd85fd488646a8c85574fd64037f32f
thebaptiste <jean-baptiste.veslin@meteo.fr>
2024-08-29 15:25:14 +0000

    build: add import tests

---
+++ integration_tests/0003_test_import_python3_extratools/import_python3_scientific.py
@@ -0,0 +1,14 @@
+import anyio
+import asgiref
+import htmltools
+import linkify_it
+import mdit_py_plugins
+import multipart #python_multipart
+import questionary
+import shiny
+import sniffio
+import starlette
+import uc_micro #uc_micro_py
+import uvicorn
+import watchfiles
+import websockets
