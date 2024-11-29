7a6de0467bc489beb6f8f804a672b16bc7c91f21
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2024-11-29 13:40:59 +0100

    build: fix for ncl-ncar to build with gcc 11 and build scipy from sources with gcc 11 (#697)

--- layers/layer1_scientific/0090_ncl_ncar/Makefile.mk
+++ layers/layer1_scientific/0090_ncl_ncar/Makefile.mk
@@ -11,13 +11,14 @@ NCL (NCAR Command Language) is a scripting language for the analysis and visuali
 WEBSITE=https://www.ncl.ucar.edu/
 LICENSE=Apache 2.0
 
+#inspired by ncl.spec in https://download-ib01.fedoraproject.org/pub/epel/9/Everything/source/tree/Packages/n/ncl-6.6.2-24.el9.src.rpm
+
 RPM_OPT_FLAGS=$(shell outside rpm -E %optflags)
-FCOPTIONS=$(RPM_OPT_FLAGS) -fPIC -fno-second-underscore -fno-range-check -fopenmp -DH5_USE_110_API
+FCOPTIONS=$(RPM_OPT_FLAGS) -fPIC -fno-second-underscore -fno-range-check -fopenmp -fallow-argument-mismatch -fcommon -DH5_USE_110_API
 CCOPTIONS=$(RPM_OPT_FLAGS) -std=c99 -fPIC -fno-strict-aliasing -fopenmp -fcommon -DH5_USE_110_API
 
 all::$(PREFIX)/bin/ncl
 $(PREFIX)/bin/ncl:
-	#inspired by ncl.spec in https://download-ib01.fedoraproject.org/pub/epel/8/Everything/SRPMS/Packages/n/ncl-6.6.2-12.el8.src.rpm
 	mkdir -p $(PREFIX)/share/ncarg
 	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) download uncompress
 	cp Site.local build/$(NAME)-$(VERSION)/config/Site.local
