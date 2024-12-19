fea64b13cbc603f75d45908335a4e6222cb2958c
thebaptiste <jean-baptiste.veslin@meteo.fr>
2024-12-19 11:01:01 +0000

    feat: upgrade pngquant to 3.0.3 and add libimagequant (with cargo build)

--- layers/layer1_scientific/0004_pngquant/Makefile.mk
+++ layers/layer1_scientific/0004_pngquant/Makefile.mk
@@ -2,18 +2,18 @@ include ../../../adm/root.mk
 include $(MFEXT_HOME)/share/package.mk
 
 export NAME=pngquant
-export VERSION=2.18.0
+export VERSION=3.0.3
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=c722e71c3fc4aac6c378e35df2cfdd93
+export CHECKSUM=0c92f3f5c5ffb31ca490d7197aff94a0
+export VERSION_LIBIMAGEQUANT=4.3.3
 DESCRIPTION=\
 PNGQUANT is a command-line utility and a library for lossy compression of PNG images
 WEBSITE=http://www.pngquant.org/
 LICENSE=GPL v3 or later with additional copyright notices for older parts of the code
 
-#Releases >= 3 are built with rust/cargo. Upgrade ?
-#See https://crates.io/crates/pngquant/3.0.1
-
 all:: $(PREFIX)/bin/pngquant
 $(PREFIX)/bin/pngquant:
-	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) download uncompress configure build install
+	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) download uncompress
+	cd build/$(NAME)-$(VERSION) && rm -rf lib && ln -s ../../../0003_libimagequant/build/libimagequant-$(VERSION_LIBIMAGEQUANT) lib
+	cd build/$(NAME)-$(VERSION) && cargo build --release && cp -f target/release/pngquant $(PREFIX)/bin/pngquant
