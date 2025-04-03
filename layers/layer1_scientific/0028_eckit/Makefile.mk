f3a0a28201cb06cee558a298943468247ea1c866
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2025-04-03 19:19:39 +0200

    feat: bump eckit to 1.28.7 and fckit to 0.13.4 (#841)

--- layers/layer1_scientific/0028_eckit/Makefile.mk
+++ layers/layer1_scientific/0028_eckit/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include $(MFEXT_HOME)/share/package.mk
 
 export NAME=eckit
-export VERSION=1.28.1
+export VERSION=1.28.7
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=d38f5bc80eb69f28aa3e041c1098dd1f
+export CHECKSUM=7d6d8c10fce363f37a8d8df55c918911
 DESCRIPTION= ecKit is a cross-platform c++ toolkit that supports development of tools and applications at ECMWF
 WEBSITE=https://github.com/ecmwf/eckit
 LICENSE=Apache 2.0 : http://www.apache.org/licenses/LICENSE-2.0
