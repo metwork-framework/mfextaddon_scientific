05762203a5dd15d1ca583266759b87ccc50046f5
thebaptiste <jean-baptiste.veslin@meteo.fr>
2025-04-07 10:10:02 +0000

    feat: bump ffmpeg from 6.0 to 7.1.1 with patch for librsvg < 2.52

--- layers/layer1_scientific/0080_ffmpeg/Makefile.mk
+++ layers/layer1_scientific/0080_ffmpeg/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include $(MFEXT_HOME)/share/package.mk
 
 export NAME=FFmpeg
-export VERSION=6.0
+export VERSION=7.1.1
 export EXTENSION=tar.xz
 export CHECKTYPE=MD5
-export CHECKSUM=47b6c5d930937413c3e308e2fdb3dfb5
+export CHECKSUM=26f2bd7d20c6c616f31d7130c88d7250
 export EXPLICIT_NAME=ffmpeg-$(VERSION)
 DESCRIPTION=\
 FFmpeg is a collection of libraries and tools to process multimedia content such as audio, video, subtitles and related metadataœ
