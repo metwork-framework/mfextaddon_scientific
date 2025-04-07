96fc70dd203f190e732930c6c2f663cbab654295
thebaptiste <jean-baptiste.veslin@meteo.fr>
2025-04-07 15:14:17 +0000

    feat: bump g2clib from 1.6.3 to 2.1.0

--- layers/layer1_scientific/0089_g2clib/Makefile.mk
+++ layers/layer1_scientific/0089_g2clib/Makefile.mk
@@ -2,18 +2,16 @@ include ../../../adm/root.mk
 include $(MFEXT_HOME)/share/package.mk
 
 export NAME=g2clib
-export VERSION=1.6.3
-export EXTENSION=tar.bz2
+export VERSION=2.1.0
+export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=5182a27323481e29d3cd6c2cbcf7add9
+export CHECKSUM=bf11e8ff0472afd8eb04188368d9911b
+export EXPLICIT_NAME=NCEPLIBS-g2c-$(VERSION)
 DESCRIPTION=\
 G2CLIB contains "C" decoder/encoder routines for GRIB edition 2
-WEBSITE=https://www.ncl.ucar.edu/
-LICENSE=Apache 2.0
+WEBSITE=https://github.com/NOAA-EMC/NCEPLIBS-g2c
+LICENSE=LGPL 3.0
 
-#Version 1.7.0 would require to build jasper >= 2.0.25
-
-all::$(PREFIX)/lib/libg2c.a
-$(PREFIX)/lib/libg2c.a:
-	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) download uncompress build
-	cd build/$(NAME)-$(VERSION) && cp libg2c_v1.6.3.a $(PREFIX)/lib/libg2c.a && cp *.h $(PREFIX)/include
+all::$(PREFIX)/lib/libg2c.so
+$(PREFIX)/lib/libg2c.so:
+	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) EXPLICIT_NAME=$(EXPLICIT_NAME) OPTIONS="-DJASPER_LIBRARY_RELEASE=$(PREFIX)/../core/lib/libjasper.so -DJASPER_INCLUDE_DIR=$(PREFIX)/../core/include" download uncompress configure_cmake3 build_cmake install_cmake
