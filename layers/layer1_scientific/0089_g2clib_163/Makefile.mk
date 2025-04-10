977f197553657b5ed1c80f71e5e470eb850b42d9
thebaptiste <jean-baptiste.veslin@meteo.fr>
2025-04-10 14:55:04 +0000

    feat: revert: bump g2clib from 1.6.3 to 2.1.0 (for ncl)

---
+++ layers/layer1_scientific/0089_g2clib_163/Makefile.mk
@@ -0,0 +1,20 @@
+include ../../../adm/root.mk
+include $(MFEXT_HOME)/share/package.mk
+
+export NAME=g2clib
+export VERSION=1.6.3
+export EXTENSION=tar.bz2
+export CHECKTYPE=MD5
+export CHECKSUM=5182a27323481e29d3cd6c2cbcf7add9
+DESCRIPTION=\
+G2CLIB contains "C" decoder/encoder routines for GRIB edition 2
+WEBSITE=https://www.ncl.ucar.edu/
+LICENSE=Apache 2.0
+
+#Version 1.7.0 would require to build jasper >= 2.0.25
+
+all::$(PREFIX)/lib/libg2clib-1.6.3.a
+$(PREFIX)/lib/libg2clib-1.6.3.a:
+	mkdir -p $(PREFIX)/include/g2clib-1.6.3
+	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) download uncompress build
+	cd build/$(NAME)-$(VERSION) && cp libg2c_v1.6.3.a $(PREFIX)/lib/libg2clib-1.6.3.a && cp *.h $(PREFIX)/include/g2clib-1.6.3
