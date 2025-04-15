3d6a1f6357b594e7269ecb9c5e32aea864108afa
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2025-04-15 16:33:42 +0200

    build: embed lttng libraries and dependencies for aspose (#865)

---
+++ layers/layer5_python3_extratools/0001_system_libraries/Makefile.mk
@@ -0,0 +1,12 @@
+include ../../../adm/root.mk
+include ../../simple_layer.mk
+
+ifeq ($(shell cat /etc/redhat-release | grep -c "release 8"), 1)
+
+LIB_LIST = $(shell cat libraries.txt)
+$(shell mkdir $(PREFIX)/lib)
+
+all:: $(PREFIX)/lib/libjson-c.so.4
+$(PREFIX)/lib/libjson-c.so.4:
+	for lib in $(LIB_LIST); do cp -r $$lib $(PREFIX)/lib; done
+endif
