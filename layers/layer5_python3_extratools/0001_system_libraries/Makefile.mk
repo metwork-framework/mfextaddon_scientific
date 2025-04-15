16db0d91112d14c69c089d2fa7500f2bfa6fd7ed
thebaptiste <jean-baptiste.veslin@meteo.fr>
2025-04-15 14:54:24 +0000

    build: fix again

--- layers/layer5_python3_extratools/0001_system_libraries/Makefile.mk
+++ layers/layer5_python3_extratools/0001_system_libraries/Makefile.mk
@@ -6,7 +6,7 @@ ifeq ($(shell cat /etc/redhat-release | grep -c "release 8"), 1)
 LIB_LIST = $(shell cat libraries.txt)
 $(shell mkdir $(PREFIX)/lib)
 
-all:: $(PREFIX)/lib/libjson-c.so.4
-$(PREFIX)/lib/libjson-c.so.4:
+all:: $(PREFIX)/lib/liblttng-ust.so.0
+$(PREFIX)/lib/liblttng-ust.so.0:
 	for lib in $(LIB_LIST); do cp -r $$lib $(PREFIX)/lib; done
 endif
