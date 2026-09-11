e04fb5b09d719369a56d68dd1c1e1cd6dffc5429
metworkbot <metworkbot@metwork-framework.org>
2026-09-11 14:45:58 +0000

    build: sync common files from github_organization_management repository

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -19,9 +19,9 @@ rm -rf html_doc rpms .build_hash
 
 case "${BRANCH}" in
     ci* | pci*)
-        export DEP_BRANCH=integration
+        export DEP_BRANCH=integration;;
     *)
-        export DEP_BRANCH=${BRANCH}
+        export DEP_BRANCH=${BRANCH};;
 esac
 
 
